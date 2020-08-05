ce872280b9e556c66ec4f72f488b8dcf6705be36
Ben Hawks <Ben.Hawks97@gmail.com>
2020-08-05 15:49:55 -0500

    Added Plot 5c, updated plot scripts 2-5c to generate EMTrack time and full event time plots, updated readme

--- plot-3.py
+++ plot-3.py
@@ -6,6 +6,7 @@ import numpy as np
 d4a=np.loadtxt("data/results_rscaletest-gke4_nodybat.txt")
 d4b=np.loadtxt("data/results_rscaletest-gke4_nodybat_bigbat.txt")
 
+# EMTrackMichelID Time
 fig=plt.figure()
 ax=fig.add_subplot(111)
 
@@ -20,7 +21,23 @@ ax.set(title="EmTrkMichelId module proc time vs # jobs (GKE-4gpu, no dynamic bat
 #yr=[219.4,219.4]
 #ax.plot(xr, yr, color='orange',label = "without tRTis",linestyle='solid',linewidth=2)
 
-plt.legend()
-plt.tight_layout()
-plt.savefig("plot-3.png")
-plt.show()
+ax.legend()
+fig.tight_layout()
+fig.savefig("plot-3_EMTrackMichelID.png")
+fig.show()
+
+# Full Event Time
+fig2=plt.figure()
+ax2=fig2.add_subplot(111)
+
+ax2.set_xlim(0,310)
+ax2.set_ylim(0,250)
+ax2.grid(True)
+ax2.errorbar(d4a[:,0], d4a[:,1], yerr=d4a[:,2], fmt='r^--', linewidth=0.8,markersize=7, markeredgecolor='red', fillstyle='none', capsize=3, label = "w/ tRTis on GKE-4gpu, avg batch size = 235")
+ax2.errorbar(d4b[:,0], d4b[:,1], yerr=d4b[:,2], fmt='bD--', linewidth=0.8,markersize=5.5, markeredgecolor='blue', fillstyle='none', capsize=3, label = "w/ tRTis on GKE-4gpu, avg batch size = 1720")
+ax2.set(title="Full Event proc time vs # jobs (GKE-4gpu, no dynamic batching)", xlabel="number of simultaneous jobs", ylabel="processing time [seconds]")
+
+ax2.legend()
+fig2.tight_layout()
+fig2.savefig("plot-3_FullEvent.png")
+fig2.show()
--- plot-5b.py
+++ plot-5b.py
@@ -6,6 +6,7 @@ import numpy as np
 d4a=np.loadtxt("data/results_rscaletest-gke4_bigbat.txt")
 d4b=np.loadtxt("data/results_rscaletest-gke4.txt")
 
+# EmTrackMichelID Time
 fig=plt.figure()
 ax=fig.add_subplot(111)
 
@@ -20,7 +21,23 @@ ax.set(title="EmTrkMichelId module proc time vs # jobs (GKE-4gpu)", xlabel="numb
 #yr=[219.4,219.4]
 #ax.plot(xr, yr, color='orange',label = "without tRTis",linestyle='solid',linewidth=2)
 
-plt.legend()
-plt.tight_layout()
-plt.savefig("plot-5b.png")
-plt.show()
+ax.legend()
+fig.tight_layout()
+fig.savefig("plot-5b_EmTrackMichelID.png")
+fig.show()
+
+# Full Event Time
+fig2=plt.figure()
+ax2=fig2.add_subplot(111)
+
+ax2.set_xlim(0,410)
+ax2.set_ylim(0,250)
+ax2.grid(True)
+ax2.errorbar(d4b[:,0], d4b[:,1], yerr=d4b[:,2], fmt='bD--', linewidth=0.8,markersize=5.5, markeredgecolor='b', fillstyle='none', capsize=3, label = "w/ tRTis on GKE-4gpu, dyn bat On, avg bat sz = 235")
+ax2.errorbar(d4a[:,0], d4a[:,1], yerr=d4a[:,2], fmt='rv--', linewidth=0.8,markersize=7, markeredgecolor='r', fillstyle='none', capsize=3, label = "w/ tRTis on GKE-4gpu, dyn bat On, avg bat sz = 1720")
+ax2.set(title="Full Event module proc time vs # jobs (GKE-4gpu)", xlabel="number of simultaneous jobs", ylabel="processing time [seconds]")
+
+ax2.legend()
+fig2.tight_layout()
+fig2.savefig("plot-5b_FullEvent.png")
+fig2.show()
